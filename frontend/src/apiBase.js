2338f8dfcea13a346ab02ed3edc1dd0bf32b4712
Darshana Perera <91814341+darshana-perera-97@users.noreply.github.com>
2026-08-13 11:24:55 +0530

    release -anuradha

--- frontend/src/apiBase.js
+++ frontend/src/apiBase.js
@@ -7,7 +7,8 @@
  */
 // export const DEFAULT_DEV_API_URL = 'http://93.127.129.102:1240';
 // export const DEFAULT_DEV_API_URL = 'http://69.197.187.24:1249';
-export const DEFAULT_DEV_API_URL = 'https://shakya.aipulse.lk';
+export const DEFAULT_DEV_API_URL = 'https://anuradha.aipulse.lk';
+// export const DEFAULT_DEV_API_URL = 'https://shakya.aipulse.lk';
 // export const DEFAULT_DEV_API_URL = 'https://chaminda-stores.aipulse.lk';
 // export const DEFAULT_DEV_API_URL = 'http://localhost:1249';
 
